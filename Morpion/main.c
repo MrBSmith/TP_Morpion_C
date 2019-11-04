9d83d8eb036c45301006191ea0b9cad5bd0a465a
HugoBaba <hugobarette@gmail.com>
2019-11-04 14:37:47 +0100

    Rennomage des variables, pour respecter les conventions

--- Morpion/main.c
+++ Morpion/main.c
@@ -7,88 +7,88 @@
 #define CROIX 2
 
 // Declare functions
-void initializeMorpion(char cMorpionArray[3][3], char cInitValue);
-void printMorpion(char cMorpionArray[3][3]);
-int isInsideBounds(int NewMove);
-int askForX(int xNewMove);
-int askForY(int yNewMove);
-int togglePlayer(int dCurrentPlayer);
-char cWhatCharIsPlayer(int dCurrentPlayer);
-int isAlreadyTaken(char cMorpionArray[3][3], int xNewMove, int yNewMove);
-int isAPlayerWinning(char cMorpionArray[3][3]);
-void printWinner(int dWin, char cMorpionArray[3][3]);
-void printCurrentPlayer(int dCurrentPlayer);
+void initializeMorpion(char c_MorpionArray[3][3], char c_InitValue);
+void printMorpion(char c_MorpionArray[3][3]);
+int isInsideBounds(int c_NewMove);
+int askForX(int n_xNewMove);
+int askForY(int n_yNewMove);
+int togglePlayer(int n_CurrentPlayer);
+char WhatCharIsPlayer(int n_CurrentPlayer);
+int isAlreadyTaken(char c_MorpionArray[3][3], int n_xNewMove, int n_yNewMove);
+int isAPlayerWinning(char c_MorpionArray[3][3]);
+void printWinner(int n_Win, char c_MorpionArray[3][3]);
+void printCurrentPlayer(int n_CurrentPlayer);
 
-int main()
-{
+int main(){
+
     // Declare the table, which will contain the Morpion
-    char cMorpionArray[3][3];
+    char c_MorpionArray[3][3];
 
     // Declare the variables used to store the coordinates of the player's move
-    int xNewMove, yNewMove;
+    int n_xNewMove, n_yNewMove;
 
     // Declare the variable used to keep track of which player is playing now
-    int dCurrentPlayer = CROIX;
+    int n_CurrentPlayer = CROIX;
 
     // Declare the variable that stocks which player wins
-    int dWin = 0;
+    int n_Win = 0;
 
     // Declare the variable that counts the number of moves done
-    int dMoveCounter = 0;
+    int n_MoveCounter = 0;
 
     // Initialize the Morpion table by giving each cell a " " for value
-    initializeMorpion(cMorpionArray, ' ');
+    initializeMorpion(c_MorpionArray, ' ');
 
     // Procedure of a turn, looping while the game isn't finished
     do{
         // Print the table on screen
-        printMorpion(cMorpionArray);
+        printMorpion(c_MorpionArray);
 
         // Ask the player to enter the coordinates of his next move until he gives an empty location
         do{
-            printCurrentPlayer(dCurrentPlayer);
+            printCurrentPlayer(n_CurrentPlayer);
             printf("Ou voulez vous jouer?\n\n");
-            xNewMove = askForX(xNewMove);
-            yNewMove = askForY(yNewMove);
+            n_xNewMove = askForX(n_xNewMove);
+            n_yNewMove = askForY(n_yNewMove);
 
             // Give a warning message if the cell is already taken
-            if(isAlreadyTaken(cMorpionArray, xNewMove, yNewMove) == true){
+            if(isAlreadyTaken(c_MorpionArray, n_xNewMove, n_yNewMove) == true){
                 printf("Un coup a deja ete joue a cet endroit\n");
             }
-        } while(isAlreadyTaken(cMorpionArray, xNewMove, yNewMove) == true);
+        } while(isAlreadyTaken(c_MorpionArray, n_xNewMove, n_yNewMove) == true);
 
         // Store 'X' or 'O' inside the Morpion array2D at the coordinates given by the player
-        cMorpionArray[yNewMove][xNewMove] = cWhatCharIsPlayer(dCurrentPlayer);
+        c_MorpionArray[n_yNewMove][n_xNewMove] = WhatCharIsPlayer(n_CurrentPlayer);
 
         // Check for the win of one of the players, if one is winning print which one
-        dWin = isAPlayerWinning(cMorpionArray);
+        n_Win = isAPlayerWinning(c_MorpionArray);
 
         // Print on screen who is the winner if there is one
-        if(dWin != 0){
-            printWinner(dWin, cMorpionArray);
+        if(n_Win != 0){
+            printWinner(n_Win, c_MorpionArray);
         }
 
 
         // Toggle the player, so we can start the next turn
-        dCurrentPlayer = togglePlayer(dCurrentPlayer);
+        n_CurrentPlayer = togglePlayer(n_CurrentPlayer);
 
         // Increment the move counter
-        dMoveCounter++;
+        n_MoveCounter++;
 
         // Print a message in case of a draw
-        if((dMoveCounter == 9) && (dWin == 0)){
+        if((n_MoveCounter == 9) && (n_Win == 0)){
             printf("Egalite!");
             break;
         }
 
-    } while(dWin == 0);
+    } while(n_Win == 0);
     // Get out of the game loop if a player as won
 
     return 0;
 }
 
 // Initialize the array2D given in argument1 at the char value passed in argument2
-void initializeMorpion(char cMorpionArray[3][3], char cInitValue){
+void initializeMorpion(char c_MorpionArray[3][3], char c_InitValue){
 
     int i, j;
 
@@ -96,14 +96,14 @@ void initializeMorpion(char cMorpionArray[3][3], char cInitValue){
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
 
-            cMorpionArray[i][j] = cInitValue;
+            c_MorpionArray[i][j] = c_InitValue;
 
         }
     }
 }
 
 // Show the board of the Morpion on screen, by printing the array2D
-void printMorpion(char cMorpionArray[3][3]){
+void printMorpion(char c_MorpionArray[3][3]){
 
     int i, j;
 
@@ -111,7 +111,7 @@ void printMorpion(char cMorpionArray[3][3]){
     for(i = 0; i < 3; i++){
         for(j = 0; j < 3; j++){
             // Print the value of the current cell
-            printf("%c ", cMorpionArray[i][j]);
+            printf("%c ", c_MorpionArray[i][j]);
 
             // Print the vertical separation between cells
             if(j < 2){
@@ -131,9 +131,9 @@ void printMorpion(char cMorpionArray[3][3]){
 
 // Check if the input of the player is inside the bounds of the Morpion
 // if it is, return true, if not return false
-int isInsideBounds(int NewMove){
+int isInsideBounds(int c_NewMove){
 
-    if((NewMove > 3) || (NewMove < 1)){
+    if((c_NewMove > 3) || (c_NewMove < 1)){
         return false;
     } else {
         return true;
@@ -141,52 +141,52 @@ int isInsideBounds(int NewMove){
 }
 
 // Ask the player for a x coordinate for his move, and return it
-int askForX(int xNewMove){
+int askForX(int n_xNewMove){
     do{
         printf("X: ");
-        scanf("%d", &xNewMove);
+        scanf("%d", &n_xNewMove);
         fflush(stdin);
 
-        if(isInsideBounds(xNewMove) == false){
+        if(isInsideBounds(n_xNewMove) == false){
             printf("Entrez une coordonnee a l'interieur du tableau!\n");
         }
-    } while(isInsideBounds(xNewMove) == false);
+    } while(isInsideBounds(n_xNewMove) == false);
 
-    return xNewMove - 1;
+    return n_xNewMove - 1;
 }
 
 // Ask the player for a y coordinate for his move, and return it
-int askForY(int yNewMove){
+int askForY(int n_yNewMove){
     do{
         printf("Y: ");
-        scanf("%d", &yNewMove);
+        scanf("%d", &n_yNewMove);
         fflush(stdin);
 
-        if(isInsideBounds(yNewMove) == false){
+        if(isInsideBounds(n_yNewMove) == false){
             printf("Entrez une coordonnee a l'interieur du tableau!\n");
         }
-    } while(isInsideBounds(yNewMove) == false);
+    } while(isInsideBounds(n_yNewMove) == false);
 
-    return yNewMove - 1;
+    return n_yNewMove - 1;
 }
 
 // Toggle the player, so they play alternatively
-int togglePlayer(int dCurrentPlayer){
+int togglePlayer(int n_CurrentPlayer){
 
-    if(dCurrentPlayer == ROND){
-        dCurrentPlayer = CROIX;
+    if(n_CurrentPlayer == ROND){
+        n_CurrentPlayer = CROIX;
     } else{
-        dCurrentPlayer = ROND;
+        n_CurrentPlayer = ROND;
     }
 
-    return dCurrentPlayer;
+    return n_CurrentPlayer;
 }
 
 // Deduce the char 'X' or 'Y' from witch player is currently playing
-char cWhatCharIsPlayer(int dCurrentPlayer){
+char WhatCharIsPlayers(int n_CurrentPlayer){
     char whatChar;
 
-    if(dCurrentPlayer == ROND){
+    if(n_CurrentPlayer == ROND){
         whatChar = 'X';
     } else{
         whatChar = 'O';
@@ -197,9 +197,9 @@ char cWhatCharIsPlayer(int dCurrentPlayer){
 
 // Check is the place the player want to place his move is already taken or not
 // Return true if it is already taken, return false is it isn't
-int isAlreadyTaken(char cMorpionArray[3][3], int xNewMove, int yNewMove){
+int isAlreadyTaken(char c_MorpionArray[3][3], int n_xNewMove, int n_yNewMove){
 
-    if(cMorpionArray[yNewMove][xNewMove] == ' '){
+    if(c_MorpionArray[n_yNewMove][n_xNewMove] == ' '){
         return false;
     } else {
         return true;
@@ -210,27 +210,27 @@ int isAlreadyTaken(char cMorpionArray[3][3], int xNewMove, int yNewMove){
 // if no player wins, return 0
 // if the ROND player wins return ROND
 // if the CROIX player wins return CROIX
-int isAPlayerWinning(char cMorpionArray[3][3]){
+int isAPlayerWinning(char c_MorpionArray[3][3]){
     int win = false;
     int i;
 
     // Check the completion of each row and columns
     for(i = 0; i<3; i++){
         // Check the completion by the same player of each rows
-        if((cMorpionArray[i][0] == cMorpionArray[i][1]) && (cMorpionArray[i][0] == cMorpionArray[i][2]) && (cMorpionArray[i][0] != ' ')){
+        if((c_MorpionArray[i][0] == c_MorpionArray[i][1]) && (c_MorpionArray[i][0] == c_MorpionArray[i][2]) && (c_MorpionArray[i][0] != ' ')){
             win = true;
         }
         // Check the completion by the same player of each column
-        if((cMorpionArray[0][i] == cMorpionArray[1][i]) && (cMorpionArray[0][i] == cMorpionArray[2][i]) && (cMorpionArray[0][i] != ' ')){
+        if((c_MorpionArray[0][i] == c_MorpionArray[1][i]) && (c_MorpionArray[0][i] == c_MorpionArray[2][i]) && (c_MorpionArray[0][i] != ' ')){
             win = true;
         }
     }
 
     // Check the completion by the same player of each diagonals
-    if((cMorpionArray[0][0] == cMorpionArray[1][1]) && (cMorpionArray[0][0] == cMorpionArray[2][2]) && (cMorpionArray[0][0] != ' ')){
+    if((c_MorpionArray[0][0] == c_MorpionArray[1][1]) && (c_MorpionArray[0][0] == c_MorpionArray[2][2]) && (c_MorpionArray[0][0] != ' ')){
         win = true;
     }
-    if((cMorpionArray[0][2] == cMorpionArray[1][1]) && (cMorpionArray[0][2] == cMorpionArray[2][0]) && (cMorpionArray[0][2] != ' ')){
+    if((c_MorpionArray[0][2] == c_MorpionArray[1][1]) && (c_MorpionArray[0][2] == c_MorpionArray[2][0]) && (c_MorpionArray[0][2] != ' ')){
         win = true;
     }
 
@@ -239,30 +239,30 @@ int isAPlayerWinning(char cMorpionArray[3][3]){
 
 
 // Print who the winner is
-void printWinner(int dWin, char cMorpionArray[3][3]){
-    if(dWin == ROND){
+void printWinner(int n_Win, char c_MorpionArray[3][3]){
+    if(n_Win == ROND){
 
         // Print the table on screen
-        printMorpion(cMorpionArray);
+        printMorpion(c_MorpionArray);
         printf("\nLe joueur qui joue les ronds a gagné!!!\n");
 
-    } else if(dWin == CROIX){
+    } else if(n_Win == CROIX){
 
         // Print the table on screen
-        printMorpion(cMorpionArray);
+        printMorpion(c_MorpionArray);
         printf("\nLe joueur qui joue les croix a gagné!!!\n");
 
     }
 }
 
 // Print who is currently playing
-void printCurrentPlayer(int dCurrentPlayer){
+void printCurrentPlayer(int n_CurrentPlayer){
 
-    if(dCurrentPlayer == ROND){
+    if(n_CurrentPlayer == ROND){
         printf("C'est a rond de jouer\n");
-    } else if(dCurrentPlayer == CROIX){
+    } else if(n_CurrentPlayer == CROIX){
         printf("C'est a croix de jouer\n");
     } else {
-        printf("Erreur: la variable dCurrentPlayer est mal attribuée\n");
+        printf("Erreur: la variable n_CurrentPlayer est mal attribuée\n");
     }
 }
